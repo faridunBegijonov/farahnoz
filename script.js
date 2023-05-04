be0357c94aa643e68b8682fa1cbeba520d3822e1
faridunBegijonov <faridunBegijonov010@gmail.com>
2023-05-04 13:15:14 +0500

    end project

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".icon").addEventListener("click", () => {
+    document.querySelector(".mobile-menu-item").style.display = "flex"
+})
+
+document.querySelector(".mobile-menu-item").addEventListener("click", () => {
+    document.querySelector(".mobile-menu-item").style.display = "none"
+})
